856fad7a9c9519284677a58a057b263c0aeee1d2
joelepore <giuseppejoelepore@gmail.com>
2021-11-21 15:09:01 +0100

    Update welcome page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="grid h-screen bg-gradient-to-tr from-gray-100 to-gray-50 relative">
+    <div class="max-w-lg p-8 text-center text-4xl font-bold tracking-tight text-gray-700 justify-self-center self-center z-20 sm:text-6xl">
+        <!-- Welcome Heading -->
+        <h1 class="tracking-wide text-4xl uppercase">Welcome to</h1>
+        <div class="px-2 py-5">
+            <img class="w-full h-auto" src="https://kit.svelte.dev/images/svelte-kit-logotype.svg" alt="">
+        </div>
+        <p class="font-mono leading-none text-4xl">+</p>
+        <h2 class="font-black bg-clip-text text-transparent bg-gradient-to-tl from-[#44A8B3] to-[#52BAB4]">TailwindCSS!</h2>
+        <!-- Icons Footer -->
+        <div class="flex justify-center items-center gap-3 py-8">
+            <a class="w-9 h-9 transition hover:opacity-40" href="https://kit.svelte.dev/" target="_blank">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fraw.githubusercontent.com%2Fsveltejs%2Fsvelte%2F29052aba7d0b78316d3a52aef1d7ddd54fe6ca84%2Fsite%2Fstatic%2Fimages%2Fsvelte-android-chrome-512.png&f=1&nofb=1" alt="">
+            </a>
+            <a class="w-9 h-9 transition hover:opacity-40" href="https://github.com/joelepore/sveltekit-tailwind" target="_blank">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.freebiesupply.com%2Flogos%2Flarge%2F2x%2Fgithub-icon-logo-png-transparent.png&f=1&nofb=1" alt="">
+            </a>
+            <a class="w-9 h-9 transition hover:opacity-40" href="https://tailwindcss.com/" target="_blank">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.icon-icons.com%2Ficons2%2F2699%2FPNG%2F512%2Ftailwindcss_logo_icon_167923.png&f=1&nofb=1" alt="">
+            </a>
+        </div>
+    </div>
+    <!-- Background -->
+    <div class="w-full h-full absolute opacity-20">
+        <div class="absolute left-0 w-1/2 h-full bg-red-500 z-0"></div>
+        <div class="absolute right-0 w-1/2 h-full bg-gradient-to-tl from-[#44A8B3] to-[#52BAB4] z-0"></div>
+    </div> 
+</main>
